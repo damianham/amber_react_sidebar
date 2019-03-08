087dfbb6c3a88641253a49d4f1cd5431c067ec4b
Damian Hamill <damianham@gmail.com>
2019-03-08 12:06:42 +0700

    add eslint npms and fix some eslint warnings

--- app/src/assets/javascripts/__tests__/app.test.js
+++ app/src/assets/javascripts/__tests__/app.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from '../app';
 
 import renderer from 'react-test-renderer';
 
-it("renders the App", () => {
+it('renders the App', () => {
   const component = renderer.create(<HashRouter>
     <App />
   </HashRouter>)
 
-  let tree = component.toJSON();
+  const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 })
